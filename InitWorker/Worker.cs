7d694adc79d517048750fbbfe13727bf37281a7b
Alien663 <rite2844@gmail.com>
2026-02-11 11:01:40 +0800

    Fix: Fix Worker Inject Service

--- InitWorker/Worker.cs
+++ InitWorker/Worker.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SampleService1.Interfaces;
+using SampleService2.Interfaces;
 
 namespace InitWorker;
 
@@ -9,12 +12,14 @@ public class Worker: BackgroundService
     private readonly IHostApplicationLifetime _host;
     private readonly IConfiguration _config;
     private readonly ILogger<Worker> _logger;
+    private readonly IServiceScopeFactory _scopeFactory;
 
-    public Worker(IHostApplicationLifetime host, IConfiguration config, ILogger<Worker> logger)
+    public Worker(IHostApplicationLifetime host, IConfiguration config, ILogger<Worker> logger, IServiceScopeFactory scopeFactory)
     {
         _host = host;
         _config = config;
         _logger = logger;
+        _scopeFactory = scopeFactory;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -24,6 +29,16 @@ public class Worker: BackgroundService
             _logger.LogInformation("Initialization started.");
             while (!stoppingToken.IsCancellationRequested)
             {
+                using (var scope = _scopeFactory.CreateScope())
+                {
+                    // Perform initialization tasks here
+                    // For example, you can resolve services from the scope and call their methods
+                    var myService = scope.ServiceProvider.GetRequiredService<ISampleService>();
+                    var myHttpClient = scope.ServiceProvider.GetRequiredService<IMyHttpClient>();
+
+                    // Call methods on the services to perform initialization tasks
+
+                }
                 await Task.Delay(TimeSpan.FromSeconds(1), stoppingToken);
             }
             _logger.LogInformation("Initialization completed.");
